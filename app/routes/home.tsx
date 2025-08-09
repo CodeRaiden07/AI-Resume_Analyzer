fa53e45a1c2f4511db58e94c6456d9ba7914782a
Praveen <praveen.kumar.silent.2014@gmail.com>
2025-08-09 14:20:26 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+// import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeAnalyzer" },
+    { name: "description", content: "Welcome to ResumeAnalyzer!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <p>Your one-stop solution for resume analysis and improvement.</p>
+      </div>
+      
+    </section>
+  </main>;
 }
